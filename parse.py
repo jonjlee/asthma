6fb1d0f6f97e5ad584fe6ccd90d156ee0ac3e5df
Jonathan Lee <jonjlee@gmail.com>
2015-04-22 16:13:01 -0500

    Add patient ID field and output unique ICD9 codes to Excel

--- parse.py
+++ parse.py
@@ -36,10 +36,16 @@ def main():
     # Get list of ICD9 codes represented in data
     icd9_lookup = transform.to_dict(icd9, 'code', 'desc')
     icd9_codes = transform.unique_values(data, 'ICD9 Codes')
-    icd9_descs = [icd9_lookup.get(code, '[Unknown]') for code in icd9_codes]
+    icd9_descs = [{
+            'Code': code,
+            'Description': icd9_lookup.get(code, '[Unknown]')
+        } for code in icd9_codes]
+    transform.sort(icd9_descs, 'Code')
 
     # Calculate ICD9 categories for each row and use to remove records meeting exclusion criteria
-    transform.calc_field(data, 'ICD9 Categories', lambda row: [code.split('.')[0] for code in row['ICD9 Codes']])
+    transform.calc_field(data,
+        'ICD9 Categories',
+        lambda row: [code.split('.')[0] for code in row['ICD9 Codes']])
     data = transform.filter(data, lambda row: not any([
         '480' in row['ICD9 Categories'],    # Pneumonia
         '461' in row['ICD9 Categories'],    # Sinusitis
@@ -50,7 +56,10 @@ def main():
         '277.02' in row['ICD9 Codes'],      # CF with pulm
         '277.09' in row['ICD9 Codes'],      # CF other
     ]))
-    
+
+    # Generate a unique ID for each patient
+    transform.id_transform(data, 'Unit#', 'pid')
+
     # Make human readable fields
     readable = deepcopy(data)
     transform.calc_field(readable, 'Diagnosis', lambda row: ','.join(row['ICD9 Codes']))
@@ -61,8 +70,9 @@ def main():
 
     # Load
     # ------------------------------------------------------------------------
-    load.to_csv(readable, 'data.csv', ['Admit', 'LOS', 'Diagnosis'])
+    load.to_csv(readable, 'data.csv', ['pid', 'Admit', 'LOS', 'Diagnosis'])
     load.to_js(anonymized, 'data.js')
+    load.to_xls(icd9_descs, 'data.xls', ['Code', 'Description'])
 
 if __name__ == '__main__':
     main()
